13b6de89909f25837f8edfb71a78a8ac814302ef
Hang Nguyen <Nguyenhangnb2501@gmail.com>
2025-04-11 11:47:52 +0700

    Xoa file

--- Buoi03_Hang_Bai01.py
+++
@@ -1,6 +0,0 @@
-set = set()
-n = int(input("Nhap so luong phan tu muon them vao danh sach: "))
-for i in range(n):
-    j = int(input("Nhap gia tri cua danh sach: "))
-    set.add(j)
-print("Danh sach da nhap vao la: ", set)
--- Buoi03_Hang_Bai04.py
+++
@@ -1,8 +0,0 @@
-lst = []
-n = int(input("Nhap so luong phan tu muon them vao danh sach: "))
-for i in range(n):
-    j = int(input("Nhap gia tri cua danh sach: "))
-    lst.append(j)
-
-for a in lst:
-# chua lam duoc
